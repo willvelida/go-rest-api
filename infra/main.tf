32ccf757dd590aee82df7a1431b6cffe0121d6e1
Will Velida <willvelida@microsoft.com>
2022-11-27 06:56:37 +1300

    Adding terraform code

---
+++ infra/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-go-rest-api-${random_integer.ri.result}"
+  location = "australiaeast"
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "asp" {
+  name                = "asp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "webapp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.asp.id
+  https_only          = true
+  site_config {
+    always_on = true
+  }
+}
